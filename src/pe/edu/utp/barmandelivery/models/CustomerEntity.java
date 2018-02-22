89b6367f7180136b70385a938cf56c4ce2bcf942
1120888 <1120888@utp.edu.pe>
2018-02-22 16:50:20 -0500

    edit commit customerentity!!!

--- src/pe/edu/utp/barmandelivery/models/CustomerEntity.java
+++ src/pe/edu/utp/barmandelivery/models/CustomerEntity.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerEntity extends BaseEntity {
-    private static String DEFAULT_SQL = "SELECT * FROM hr.barmans";
+    private static String DEFAULT_SQL = "SELECT * FROM bar.customers";
 
     private List<Customer> findByCriteria(String sql) {
         List<Customer> customers;
@@ -16,8 +16,8 @@ public class CustomerEntity extends BaseEntity {
                 ResultSet resultSet = getConnection().createStatement().executeQuery(sql);
                 while (resultSet.next()) {
                     Customer customer = new Customer()
-                            .setId(resultSet.getInt("customer id"))
-                            .setName(resultSet.getString("cliente_name"));
+                            .setId(resultSet.getInt("customer_id"))
+                            .setName(resultSet.getString("customer_name"));
                     customers.add(customer);
                 }
                 return customers;
@@ -34,31 +34,18 @@ public class CustomerEntity extends BaseEntity {
 
     public Customer findById(int id) {
         List<Customer> customers = findByCriteria(
-                "WHERE cliente_id =" +
+                "WHERE customer_id =" +
                         String.valueOf(id));
         return (customers != null ? customers.get(0) : null);
     }
 
     public Customer findByName(String name) {
         List<Customer> customers = findByCriteria(
-                "WHERE cliente_name =" + name + "'");
+                "WHERE customer_name =" + name + "'");
         return (customers != null ? customers.get(0) : null);
     }
 
-    private int getMaxId() {
-        String sql = "SELECT MAX(cliente_id) AS max_id FROM clientes";
-        if(getConnection()!=null){
-            try {
-                ResultSet resultSet = getConnection()
-                        .createStatement()
-                        .executeQuery(sql);
-                return resultSet.next() ? resultSet.getInt("max_id") : 0;
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return 0;
-    }
+
 
     private int updatebyCriteria (String sql){
         if (getConnection() != null){
@@ -73,31 +60,18 @@ public class CustomerEntity extends BaseEntity {
         return 0;
     }
 
-    public Customer create (String name){
-        if (findByName(name) == null) {
-            if (getConnection() != null) {
-                String sql = "INSER INTO clientes(cliente_id,cliente_name)" + "VALUES(" + String.valueOf(getMaxId() + 1) + ",'"
-                        + name + "')";
-                int results = updatebyCriteria(sql);
-                if (results > 0) {
-                    Customer customer = new Customer(getMaxId(), name);
-                    return customer;
-                }
-            }
-        }
-        return null;
-    }
+
     public boolean delete(int id){
-        return updatebyCriteria("DELETE FROM clientes WHERE cliente_id ="+String.valueOf(id)) > 0;
+        return updatebyCriteria("DELETE FROM customers WHERE customer_id ="+String.valueOf(id)) > 0;
 
     }
 
     public boolean delete(String name){
-        return updatebyCriteria("DELETE FROM clientes WHERE cliente_name ='"+name+"'")>0;
+        return updatebyCriteria("DELETE FROM customers WHERE customer_name ='"+name+"'")>0;
     }
 
     public boolean update(Customer customer){
-        return updatebyCriteria("UPDATE clientes SET cliente_name ='"+ customer.getName()+"'WHERE cliente_id="+
+        return updatebyCriteria("UPDATE customers SET customer_name ='"+ customer.getName()+"'WHERE customer_id="+
                 String.valueOf(customer.getId()))>0;
     }
 
