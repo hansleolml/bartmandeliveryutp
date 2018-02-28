f2d20a0df421538aa2b00801fb4640a1a594eb2c
jcotrina31 <1410763@utp.edu.pe>
2018-02-28 14:25:40 -0500

    modificacndo conexcion a base de datos

--- src/pe/edu/utp/barmandelivery/models/BarService.java
+++ src/pe/edu/utp/barmandelivery/models/BarService.java
@@ -16,7 +16,7 @@ public class BarService {
         if(connection == null) {
             try {
                 connection = ((DataSource) InitialContext
-                        .doLookup("jdbc/MySQLDataSource1"))
+                        .doLookup("jdbc/MySQLDataSource2"))
                         .getConnection();
             } catch (NamingException | SQLException e) {
                 e.printStackTrace();
